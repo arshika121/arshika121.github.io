f113de240576f17080000a9ccd3b062bdd3b8b80
arshika121 <rvarshika160@gmail.com>
2021-05-03 15:38:21 +0530

    third commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container d-flex container-box">
+      <UserInputElements
+        :formData="formData"
+        @changeFormData="changeFormData($event)"
+        @onFileChange="onFileChange($event)"
+      />
+      <GeneratedCardComponent :formData="formData" :image="image" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import UserInputElements from "./components/UserInputElements";
+import GeneratedCardComponent from "./components/GeneratedCardComponent";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    UserInputElements,
+    GeneratedCardComponent,
+  },
+  data() {
+    return {
+      formData: {
+        firstName: "",
+        lastName: "",
+        department: "",
+        academicYear: "",
+      },
+
+      image: "",
+    };
+  },
+  methods: {
+    changeFormData(keyObject) {
+      console.log("Parent Element Value: ", keyObject);
+      this.formData = Object.assign({}, this.formData, keyObject);
+    },
+    onFileChange(images) {
+      this.image = images;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-weight: 500;
+}
+.form-input {
+  min-width: 25em;
+  padding: 3%;
+  border: 1px solid #c5c0c0;
+  border-radius: 6px;
+}
+.card-input {
+  position: relative;
+  margin: 20px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.card-name {
+  top: 50px;
+}
+.academic-year {
+  top: 40px;
+}
+.department-name {
+  top: 70px;
+}
+.section-container {
+  min-width: 550px;
+  height: 585px;
+  border: 1px solid black;
+  box-sizing: border-box;
+}
+.card-container {
+  position: relative;
+  max-width: 55%;
+  margin: 0px;
+  padding: 14%;
+  top: 100px;
+  left: 110px;
+  border: 1px solid black;
+}
+.circleBase {
+  border-radius: 50%;
+  position: absolute;
+  height: 200px;
+  width: 200px;
+  border: 1px solid #000;
+}
+
+.circle1 {
+  background: white;
+}
+.circle2 {
+  left: 835px;
+  top: 0px;
+  background: white;
+}
+button {
+  position: relative;
+  top: 180px;
+  width: 200px;
 }
 </style>
